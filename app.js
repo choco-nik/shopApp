fdd87a32bea0d024109a6f1b816169ae396c6e64
eergand <eergand@gmail.com>
2021-10-11 16:35:07 +0100

    Sixth Commit

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.delete("/categories/:id", async (req,res) => {
     const categoryID = req.params.id;
     const category = await Category.findByPk(categoryID);
     if (!category){
-        return res.sendStatus(400);
+        return res.sendStatus(404);
     }
     await category.destroy();
     res.sendStatus(200);
@@ -45,10 +45,58 @@ app.put("/categories/:id", async (req,res) => {
     const categoryID = req.params.id;
     const category = await Category.findByPk(categoryID);
     if (!category){
-        return res.sendStatus(400);
+        return res.sendStatus(404);
     }
     await category.update(req.body);
     res.status(200).json(category);
+});
+
+// Get all the item in a category
+app.get("/categories/:id/items", async (req,res) => {
+    const items = await Item.findAll();
+    res.json(items);
+});
+
+// Get an item in a category
+app.get("/items/:id", async (req,res) => {
+    const categoryID = req.params.id;
+    const item = await Item.findByPk(categoryID);
+    if (!item){
+        return res.sendStatus(404);
+    }
+    res.status(200).json(item);
+});
+
+// Create an item in a category
+app.post("/categories/:id/items", async (req, res) => {
+    const categoryID = req.params.id;
+    const category = await Category.findByPk(categoryID);
+
+    const { title, imageUrl, price, description } = req.body;
+    await category.createItem({ title, imageUrl, price, description });
+    res.status(201).json(category);
+});
+
+// Delete an item by its ID
+app.delete("/items/:id", async (req,res) => {
+    const itemID = req.params.id;
+    const item = await Item.findByPk(itemID);
+    if (!item){
+        return res.sendStatus(404);
+    }
+    await item.destroy();
+    res.sendStatus(200);
+});
+
+// Replace an item by its ID
+app.put("/items/:id", async (req,res) => {
+    const categoryID = req.params.id;
+    const item = await Item.findByPk(categoryID);
+    if (!item){
+        return res.sendStatus(404);
+    }
+    await item.update(req.body);
+    res.status(200).json(item);
 });
 
 setupDb();
